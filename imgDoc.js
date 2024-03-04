3edc571b492d78e329b778713059bd288ee74e86
黃阿皓 <49857978+haohows@users.noreply.github.com>
2024-03-04 11:43:46 +0800

    Add files via upload

--- imgDoc.js
+++ imgDoc.js
@@ -1,21 +1,18 @@
 const imgdata = [
     {
+
         file: "pkc.jpg",
+
     },
     {
+
         file: "duck.jpg",
+
     },
     {
+
         file: "cat.jpg",
-    },
-    {
-        file: "cat2.png",
-    },
-    {
-        file: "dog.jpg",
-    },
-    {
-        file: "sasa.jpg",
+
     },
 ]
 
